22f9a081592d074feffd6d384ee0af72f1754ba7
Shayan <93779726+ShayanAhmed-0@users.noreply.github.com>
2022-09-29 06:18:41 +0500

    34.Pizzas
    Pizzas: Think of at least three kinds of your favorite pizza. Store these pizza names in a array, and then use a for loop to print the name of each pizza.
    • Modify your for loop to print a sentence using the name of the pizza instead of printing just the name of the pizza. For each pizza you should have one line of output containing a simple statement like I like pepperoni pizza.
    • Add a line at the end of your program, outside the for loop, that states how much you like pizza. The output should consist of three or more lines about the kinds of pizza you like and then an additional sentence, such as I really love pizza!

---
+++ JavaScript/34.Pizzas.js
@@ -0,0 +1,14 @@
+let pizza =["Margherita Pizza","BBQ Chicken Pizza","Pepperoni Pizza"]
+for (let i = 0; i < pizza.length; i++) {
+    if(pizza[i]=="Margherita Pizza"){
+        console.log("I Like Margherita Pizza")
+    }
+    else if(pizza[i]=="BBQ Chicken Pizza"){
+        console.log("I Like BBQ Chicken Pizza")
+    }
+    else{
+        console.log("I Like Pepperoni Pizza")
+    }
+    
+}
+console.log("I really love pizza!")
