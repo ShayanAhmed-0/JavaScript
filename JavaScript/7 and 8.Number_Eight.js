c34318fc7425ab4778700172fca06dafdc81ed93
Shayan <93779726+ShayanAhmed-0@users.noreply.github.com>
2022-09-29 06:02:05 +0500

    Update 7 and 8.Number_Eight.js

--- JavaScript/7 and 8.Number_Eight.js
+++ JavaScript/7 and 8.Number_Eight.js
@@ -1,3 +1,7 @@
+/*
+Name :Shayan Ahmed Date:9/29/2022
+it uses adddition substraction multiplication and division operators to get number 8 as a result
+*/
 console.log(5 + 3)
 console.log(10 - 2)
 console.log(4 * 2)
