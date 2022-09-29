b77134eb023e90984861dcec5f0a55ee24ac29d4
Shayan <93779726+ShayanAhmed-0@users.noreply.github.com>
2022-09-29 14:22:34 +0500

    44.sandwitches fixed

--- JavaScript/44.Sandwiches.js
+++ JavaScript/44.Sandwiches.js
@@ -1,6 +1,8 @@
 function sandwich(item) {
     console.log(item);
 }
-sandwich("salad")
-sandwich("mayonnaise")
-sandwich("ketchup")
+let item=["salad","mayonnaise","ketchup"]
+console.log("summary of the sandwich that is being ordered");
+sandwich(item[0])
+sandwich(item[1])
+sandwich(item[2])
