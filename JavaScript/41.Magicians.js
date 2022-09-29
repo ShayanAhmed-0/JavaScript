f95091c39e7796968b44e8f88273bf1eb62d832b
Shayan <93779726+ShayanAhmed-0@users.noreply.github.com>
2022-09-29 06:21:42 +0500

    41.Magicians
    Magicians: Make a array of magician’s names. Pass the array to a function called show_magicians(), which prints the name of each magician in the array.

---
+++ JavaScript/41.Magicians.js
@@ -0,0 +1,7 @@
+function show_magicians(name) {
+    console.log(name)
+}
+let array=["David Blaine","Penn and Teller","Jerry Sadowitz","Dynamo","Apollo Robbins"]
+for (let i = 0; i < array.length; i++) {
+    show_magicians(array[i])
+}
