6ce2f6540a5417b2c1ae832174e8f2cce61f9a0b
Shayan <93779726+ShayanAhmed-0@users.noreply.github.com>
2022-09-29 06:04:45 +0500

    12.Greetings
    Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.

---
+++ JavaScript/12.Greeting.js
@@ -0,0 +1,5 @@
+const Names = ["Sameer", "Bilal", "Shumail"];
+
+console.log("hello " + Names[0] + " how are you?");
+console.log("hello " + Names[1] + " how are you?");
+console.log("hello " + Names[2] + " how are you?");
