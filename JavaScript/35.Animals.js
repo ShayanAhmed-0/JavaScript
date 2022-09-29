d8e456c4009163fcaabe9d47f078442b37ba89b1
Shayan <93779726+ShayanAhmed-0@users.noreply.github.com>
2022-09-29 06:19:03 +0500

    35.Animals
    Animals: Think of at least three different animals that have a common characteristic. Store the names of these animals in a list, and then use a for loop to print out the name of each animal. • Modify your program to print a statement about each animal, such as A dog would make a great pet. • Add a line at the end of your program stating what these animals have in common. You could print a sentence such as Any of these animals would make a great pet!

---
+++ JavaScript/35.Animals.js
@@ -0,0 +1,14 @@
+let Animals=["Cat","Dog","Rabbit"]
+for (let i = 0; i < Animals.length; i++) {
+    if(pizza[i]=="Cat"){
+        console.log("A Cat would make a great pet.")
+    }
+    else if(pizza[i]=="Dog"){
+        console.log("A Dog would make a great pet.")
+    }
+    else{
+        console.log("A Rabbit would make a great pet.")
+    }
+    
+}
+console.log("Any of these animals would make a great pet!")
