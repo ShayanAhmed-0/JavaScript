16ee064f8fef605635e9dfaa0acfd994b21cfc2b
Shayan <93779726+ShayanAhmed-0@users.noreply.github.com>
2022-09-29 06:09:29 +0500

    20.StoreInArray
    Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.

---
+++ JavaScript/20.StoreInArray.js
@@ -0,0 +1,7 @@
+let mountains=["Mount Everest","K2"]
+let rivers=["Jhelum","Chenab","Indus"]
+let countries=["Pakistan","Australia","Germany"]
+let cities=["Karachi","Lahore","Islamabad"]
+let languages=["Urdu","English"]
+let list=mountains.concat(rivers,countries,cities,languages)
+console.log(list)
