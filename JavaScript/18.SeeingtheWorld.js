b110a08356ac93fd83ebb518325fa5a8785c8c2c
Shayan <93779726+ShayanAhmed-0@users.noreply.github.com>
2022-09-29 06:07:58 +0500

    18.Seeing the World
    Seeing the World: Think of at least five places in the world you’d like to visit.
    • Store the locations in a array. Make sure the array is not in alphabetical order.
    • Print your array in its original order.
    • Print your array in alphabetical order without modifying the actual list.
    • Show that your array is still in its original order by printing it.
    • Print your array in reverse alphabetical order without changing the order of the original list.
    • Show that your array is still in its original order by printing it again.
    • Reverse the order of your list. Print the array to show that its order has changed.
    • Reverse the order of your list again. Print the list to show it’s back to its original order.
    • Sort your array so it’s stored in alphabetical order. Print the array to show that its order has been changed.
    • Sort to change your array so it’s stored in reverse alphabetical order. Print the list to show that its order has changed.

---
+++ JavaScript/18.SeeingtheWorld.js
@@ -0,0 +1,18 @@
+let places =["Ha Long Bay","The Colosseum","The Amazon rainforest","The pyramids of Giza","Grand Canyon"]
+console.log(places)
+let alphabetical=Array.from(places)
+alphabetical.sort();
+console.log(places)
+console.log(alphabetical)
+let reverse_alphabetical=Array.from(places)
+reverse_alphabetical.reverse();
+console.log(places)
+console.log(reverse_alphabetical)
+places.reverse()
+console.log(places)
+places.reverse()
+console.log(places)
+places.sort()
+console.log(places)
+places.reverse()
+console.log(places)
