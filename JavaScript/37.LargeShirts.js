ee4a3088691841d9809049f697ded4c0c89428eb
Shayan <93779726+ShayanAhmed-0@users.noreply.github.com>
2022-09-29 06:19:51 +0500

    37.Large Shirts
    Large Shirts: Modify the make_shirt() function so that shirts are large by default with a message that reads I love JavaScript. Make a large shirt and a medium shirt with the default message, and a shirt of any size with a different message.

---
+++ JavaScript/37.LargeShirts.js
@@ -0,0 +1,6 @@
+function make_shirt(size,txt) {
+    console.log("Size of T-Shirt is:"+size+" message printed on it is:"+txt)
+}
+make_shirt("Large","I love JavaScript")
+make_shirt("Medium","I love JavaScript")
+make_shirt("small","Hello World")
