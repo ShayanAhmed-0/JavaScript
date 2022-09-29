d2081eae7a58d068173da893aece376f96cdea9f
Shayan <93779726+ShayanAhmed-0@users.noreply.github.com>
2022-09-29 06:05:39 +0500

    14.Guests List
    Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner.

---
+++ JavaScript/14.GuestList.js
@@ -0,0 +1,5 @@
+const guestList=["Sameer","Bilal","Shumail"]
+for (let index = 0; index < guestList.length; index++) {
+    let invite= "Hey "+ guestList[index]+ "  you are invited for dinner at our house"
+    console.log(invite);   
+}
